127e05a64cf6c143a2422c4139c43bc85b6a4db4
Alperen Lermi <alermi@k2m17s03.42kocaeli.com.tr>
2024-10-20 13:18:00 +0300

    final

---
+++ dsfsdfsdf/ft_memmove.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alermi <alermi@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/17 18:47:40 by alermi            #+#    #+#             */
+/*   Updated: 2024/10/20 11:02:55 by alermi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*memmove(void *dest, const void *src, size_t n)
+{
+	char	*c_dest;
+	char	*c_src;
+	size_t	i;
+
+	if (!dest && !src)
+		return (NULL);
+	c_dest = (char *)dest;
+	c_src = (char *)src;
+	i = 0;
+	if (c_dest > c_src)
+	{
+		while (n-- > 0)
+			c_dest[n] = c_src[n];
+	}
+	else
+	{
+		while (i++ < n)
+			c_dest[i] = c_src[i];
+	}
+	return (dest);
+}
---
+++ dsfsdfsdf/libft.h
@@ -0,0 +1,23 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <stdlib.h>
+
+//write the MAX_INT_MIN
+
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*memmove(void *dest, const void *src, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+size_t	ft_strlen(const char *c);
+//size_t	ft_strlcpy(char *dest, const char *src, size_t len);
+
+#endif
